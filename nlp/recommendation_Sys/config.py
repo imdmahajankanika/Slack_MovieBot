501da145812897aa4302b62faae16269877316c5
imdmahajankanika <48632517+imdmahajankanika@users.noreply.github.com>
2021-03-15 06:15:53 +0100

    commit

--- nlp/recommendation_Sys/config.py
+++ nlp/recommendation_Sys/config.py
@@ -7,7 +7,7 @@ from slackclient import SlackClient
 
 ################# Slack configuration   ##########################
 
-SLACK_BOT_TOKEN='xoxb-1849778358129-1864408962976-vpUP71B8n0IJqZ0qpDGvV9hn'
+SLACK_BOT_TOKEN='xoxb-1849778358129-1864408962976-lDvoiy1eF7rSimvVPL1zgSjU'
 SLACK_VERIFICATION_TOKEN='sxTkZCZFqMczL4AC0SWt6Udw' 
 
 # instantiate Slack client
